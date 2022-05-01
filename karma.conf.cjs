70084402967e970a4cf1d57c713ce8c54f6127e7
Julian Grinblat <julian@dotcore.co.il>
2022-05-01 15:12:32 +0900

    Try again

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -60,6 +60,7 @@ module.exports = (config) => {
 
     rollupPreprocessor: {
       plugins: [
+        require('rollup-plugin-istanbul')({ exclude: ['test/*.js'] }),
         require('@rollup/plugin-alias')({ entries: { http: './test/http-polyfill.js' } }),
         require('@rollup/plugin-node-resolve').default({
           mainFields: ['module', 'browser', 'main'],
