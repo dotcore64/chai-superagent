7fad2fabc1c276b9511c80838a4730544891adb3
Julian Grinblat <julian@dotcore.co.il>
2026-05-26 16:29:55 +0900

    chore: add prettier (#503)

--- test/integration.js
+++ test/integration.js
@@ -1,175 +1,194 @@
-import { createServer } from 'node:http';
-import { env } from 'node:process';
-import { expect, use } from 'chai';
-import request from 'superagent';
-import prefix from 'superagent-prefix';
+import { createServer } from "node:http";
+import { env } from "node:process";
+import { expect, use } from "chai";
+import request from "superagent";
+import prefix from "superagent-prefix";
 
 // eslint-disable-next-line import/no-unresolved
-import superagent from 'chai-superagent';
+import superagent from "chai-superagent";
 
 use(superagent());
 
 // https://github.com/ladjs/superagent/pull/1805
 // https://github.com/ladjs/superagent/pull/1829
 const { SUPERAGENT_VERSION } = env;
-const BASEURL = env.HTTPBIN_BASEURL ?? 'https://httpbin.org';
+const BASEURL = env.HTTPBIN_BASEURL ?? "https://httpbin.org";
 
-const normalizeAddress = ({ address, family, port }) => `http://${(SUPERAGENT_VERSION === '9'
-  ? address.replace('::', 'localhost')
-  : (family === 'IPv6'
-    ? `[${address}]`
-    : address
-  ))}:${port}/`;
+const normalizeAddress = ({ address, family, port }) =>
+  `http://${
+    SUPERAGENT_VERSION === "9"
+      ? address.replace("::", "localhost")
+      : family === "IPv6"
+        ? `[${address}]`
+        : address
+  }:${port}/`;
 
-describe('superagent', () => {
-  const isNode = typeof process === 'object';
+describe("superagent", () => {
+  const isNode = typeof process === "object";
   // eslint-disable-next-line unicorn/prefer-global-this
-  const isBrowser = typeof window === 'object';
+  const isBrowser = typeof window === "object";
 
-  describe('Browser and Node.js', () => {
-    it('can request a web page', () => request
-      .get(`${BASEURL}/html`)
-      .then((res) => {
+  describe("Browser and Node.js", () => {
+    it("can request a web page", () =>
+      request.get(`${BASEURL}/html`).then((res) => {
         expect(res).to.have.status(200);
-        expect(res).to.be.html;  
-        expect(res).to.not.be.text;  
-        expect(res).to.not.be.json;  
-        expect(res.text).to.be.a('string').with.length.above(0);
+        expect(res).to.be.html;
+        expect(res).to.not.be.text;
+        expect(res).to.not.be.json;
+        expect(res.text).to.be.a("string").with.length.above(0);
 
         // Slightly different behavior in SuperAgent in Node/browsers
-        isNode && expect(res.body).to.deep.equal({});  
-        isBrowser && expect(res.body).to.be.null;  
+        isNode && expect(res.body).to.deep.equal({});
+        isBrowser && expect(res.body).to.be.null;
       }));
 
-    it('can request JSON data', () => request
-      .get(`${BASEURL}/get`)
-      .then((res) => {
+    it("can request JSON data", () =>
+      request.get(`${BASEURL}/get`).then((res) => {
         expect(res).to.have.status(200);
-        expect(res).to.be.json;  
-        expect(res).to.not.be.html;  
-        expect(res).to.not.be.text;  
-        expect(res.text).to.be.a('string').with.length.above(0);
-        expect(res.body).to.be.an('object');
+        expect(res).to.be.json;
+        expect(res).to.not.be.html;
+        expect(res).to.not.be.text;
+        expect(res.text).to.be.a("string").with.length.above(0);
+        expect(res.body).to.be.an("object");
       }));
 
-    it('can read response headers', () => request
-      .get(`${BASEURL}/response-headers`)
-      .query({ 'content-type': 'application/json' })
-      .query({ pragma: 'test1' })
-      .query({ location: 'test2' })
-      .query({ 'x-api-key': 'test3' })
-      .then((res) => {
-        expect(res).to.have.status(200);
+    it("can read response headers", () =>
+      request
+        .get(`${BASEURL}/response-headers`)
+        .query({ "content-type": "application/json" })
+        .query({ pragma: "test1" })
+        .query({ location: "test2" })
+        .query({ "x-api-key": "test3" })
+        .then((res) => {
+          expect(res).to.have.status(200);
 
-        // Content-Type and Pragma are supported on Node and browser
-        expect(res).to.be.json;  
-        expect(res).to.have.header('Content-Type', /json$/);
-        expect(res).to.have.header('Pragma', 'test1');
+          // Content-Type and Pragma are supported on Node and browser
+          expect(res).to.be.json;
+          expect(res).to.have.header("Content-Type", /json$/);
+          expect(res).to.have.header("Pragma", "test1");
 
-        // When running in a browser, only "simple" headers are readable
-        // https://www.w3.org/TR/cors/#simple-response-header
-        isNode && expect(res).to.have.header('Location', 'test2');  
-        isNode && expect(res).to.have.header('X-API-Key', 'test3');  
-        isBrowser && expect(res).to.not.have.header('Location');  
-        isBrowser && expect(res).to.not.have.header('X-API-Key');  
-      }));
+          // When running in a browser, only "simple" headers are readable
+          // https://www.w3.org/TR/cors/#simple-response-header
+          isNode && expect(res).to.have.header("Location", "test2");
+          isNode && expect(res).to.have.header("X-API-Key", "test3");
+          isBrowser && expect(res).to.not.have.header("Location");
+          isBrowser && expect(res).to.not.have.header("X-API-Key");
+        }));
 
-    it('succeeds when response has an error status', () => request
-      .get(`${BASEURL}/status/400`)
-      .ok((res) => res.status === 400)
-      .then((res) => {
-        expect(res).to.have.status(400);
-      }));
+    it("succeeds when response has an error status", () =>
+      request
+        .get(`${BASEURL}/status/400`)
+        .ok((res) => res.status === 400)
+        .then((res) => {
+          expect(res).to.have.status(400);
+        }));
 
-    it('should reject non valid instances in strict mode', () => {
-      expect(
-        () => expect({}).to.have.status(200),
-      ).to.throw(Error, 'expected {} to be an instance of Response');
+    it("should reject non valid instances in strict mode", () => {
+      expect(() => expect({}).to.have.status(200)).to.throw(
+        Error,
+        "expected {} to be an instance of Response",
+      );
 
-      expect(
-        () => expect({}).to.be.json,
-      ).to.throw(Error, 'expected {} to be an instance of Request or Response');
+      expect(() => expect({}).to.be.json).to.throw(
+        Error,
+        "expected {} to be an instance of Request or Response",
+      );
 
-      expect(
-        () => expect({}).to.be.html,
-      ).to.throw(Error, 'expected {} to be an instance of Request or Response');
+      expect(() => expect({}).to.be.html).to.throw(
+        Error,
+        "expected {} to be an instance of Request or Response",
+      );
 
-      expect(
-        () => expect({}).to.be.text,
-      ).to.throw(Error, 'expected {} to be an instance of Request or Response');
+      expect(() => expect({}).to.be.text).to.throw(
+        Error,
+        "expected {} to be an instance of Request or Response",
+      );
 
-      expect(
-        () => expect({}).to.have.header('Pragma', 'test1'),
-      ).to.throw(Error, 'expected {} to be an instance of Request or Response');
+      expect(() => expect({}).to.have.header("Pragma", "test1")).to.throw(
+        Error,
+        "expected {} to be an instance of Request or Response",
+      );
 
-      expect(
-        () => expect({}).to.redirect,
-      ).to.throw(Error, 'expected {} to be an instance of Response');
+      expect(() => expect({}).to.redirect).to.throw(
+        Error,
+        "expected {} to be an instance of Response",
+      );
 
-      expect(
-        () => expect({}).to.redirectTo('foo'),
-      ).to.throw(Error, 'expected {} to be an instance of Response');
+      expect(() => expect({}).to.redirectTo("foo")).to.throw(
+        Error,
+        "expected {} to be an instance of Response",
+      );
 
-      expect(
-        () => expect({}).to.have.param('foo', 'bar'),
-      ).to.throw(Error, 'expected {} to be an instance of Request');
+      expect(() => expect({}).to.have.param("foo", "bar")).to.throw(
+        Error,
+        "expected {} to be an instance of Request",
+      );
 
-      expect(
-        () => expect({}).to.have.cookie('foo', 'bar'),
-      ).to.throw(Error, 'expected {} to be an instance of Request');
+      expect(() => expect({}).to.have.cookie("foo", "bar")).to.throw(
+        Error,
+        "expected {} to be an instance of Request",
+      );
 
-      expect(
-        () => expect({}).to.have.cookie('foo', 'bar'),
-      ).to.throw(Error, 'expected {} to be an instance of Request');
+      expect(() => expect({}).to.have.cookie("foo", "bar")).to.throw(
+        Error,
+        "expected {} to be an instance of Request",
+      );
 
-      expect(
-        () => expect({}).to.have.charset('utf8'),
-      ).to.throw(Error, 'expected {} to be an instance of Request');
+      expect(() => expect({}).to.have.charset("utf8")).to.throw(
+        Error,
+        "expected {} to be an instance of Request",
+      );
     });
   });
 
   if (isNode) {
-    describe('Node.js', () => {
-      it('can request an already existing url', (done) => {
+    describe("Node.js", () => {
+      it("can request an already existing url", (done) => {
         const server = createServer((req, res) => {
-          expect(req.headers['x-api-key']).to.equal('test2');
-          res.writeHeader(200, { 'content-type': 'text/plain' });
-          res.end('hello world');
+          expect(req.headers["x-api-key"]).to.equal("test2");
+          res.writeHeader(200, { "content-type": "text/plain" });
+          res.end("hello world");
         });
 
         server.listen(0, () => {
           request
             .get(normalizeAddress(server.address()))
-            .set('X-API-Key', 'test2')
+            .set("X-API-Key", "test2")
             .end((err, res) => {
               expect(res).to.have.status(200);
-              expect(res.text).to.equal('hello world');
-              server.once('close', () => { done(err); });
+              expect(res.text).to.equal("hello world");
+              server.once("close", () => {
+                done(err);
+              });
               server.close();
             });
         });
       });
 
-      it('agent can be used to persist cookies', (done) => {
+      it("agent can be used to persist cookies", (done) => {
         const server = createServer((req, res) => {
-          res.setHeader('Set-Cookie', 'mycookie=test');
-          res.writeHeader(200, { 'content-type': 'text/plain' });
+          res.setHeader("Set-Cookie", "mycookie=test");
+          res.writeHeader(200, { "content-type": "text/plain" });
           res.end(`your cookie: ${req.headers.cookie}`);
         });
 
         server.listen(0, () => {
-          const agent = request.agent().use(prefix(normalizeAddress(server.address())));
+          const agent = request
+            .agent()
+            .use(prefix(normalizeAddress(server.address())));
 
           agent
-            .get('/')
+            .get("/")
             .then((res) => {
-              expect(res.headers['set-cookie'][0]).to.equal('mycookie=test');
-              expect(res.text).to.equal('your cookie: undefined');
+              expect(res.headers["set-cookie"][0]).to.equal("mycookie=test");
+              expect(res.text).to.equal("your cookie: undefined");
             })
-            .then(() => agent.get('/'))
+            .then(() => agent.get("/"))
             .then((res) => {
-              expect(res.text).to.equal('your cookie: mycookie=test');
-              server.once('close', () => { done(); });
+              expect(res.text).to.equal("your cookie: mycookie=test");
+              server.once("close", () => {
+                done();
+              });
               server.close();
             });
         });
